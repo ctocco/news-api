9bcdc89a37a5d35dbf97eb02e622bc292da892b7
Claudio Tocco <tocco.a.claudio@gmail.com>
2019-01-25 15:52:33 +0100

    adding folder structure to project

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import Header from "./Header";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Header extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <p>Hello</p>
+      </div>
+    );
+  }
+}
+
+export default Header;
